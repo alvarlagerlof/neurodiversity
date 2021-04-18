0aa94d71a0bc1fe433e7f99138e0ed22c5b9a249
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-04-18 22:29:07 +0200

    Remove t

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
 
   return (
     <div className={styles.container}>
-      t
       <Head>
         <title>{title}</title>
         <meta name="description" content={description} />
