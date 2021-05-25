48877fb16ba59edef13f9aa92b509785e59ec3e0
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-25 14:59:25 +0200

    Add alt to icon

--- components/blocks/IconHeading.js
+++ components/blocks/IconHeading.js
@@ -1,7 +1,7 @@
-export default function IconHeading({ url, children }) {
+export default function IconHeading({ url, alt, children }) {
   return (
     <div className="flex flex-row space-x-2 mb-0">
-      <img src={url} alt="Cross icon" />
+      <img src={url} alt={alt} />
       <h3 className="font-display text-xl md:text-2xl">{children}</h3>
     </div>
   );
