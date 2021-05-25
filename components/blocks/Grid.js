a924089d02805c4dabf699eb39226d50111fcd6f
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-25 04:23:12 +0200

    Grid smaller on mobile

--- components/blocks/Grid.js
+++ components/blocks/Grid.js
@@ -1,6 +1,6 @@
 export default function Grid({ children }) {
   return (
-    <div className="grid grid-cols-3 gap-4 items-center lg:-m-4">
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 items-center lg:-m-4">
       {children}
     </div>
   );
