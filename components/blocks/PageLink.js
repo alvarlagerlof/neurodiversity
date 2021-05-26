8ba09eee969ef7deab1f8386fdba65798db21568
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-26 14:46:53 +0200

    PageLink allowing disabled links

--- components/blocks/PageLink.js
+++ components/blocks/PageLink.js
@@ -1,13 +1,19 @@
-export default function PageLink({ title, description, href }) {
+export default function PageLink({ title, description, href, disabled }) {
   return (
-    <a
-      href={href}
-      className="flex flex-col h-full items-center justify-center bg-gray-200 rounded-xl p-4 space-y-1 hover:ring hover:ring-primary-light focus:outline-none focus:ring focus:ring-primary-light "
-    >
-      <h2 className="text-center font-display font-semibold text-xl">
-        {title}
-      </h2>
-      <p className="text-center">{description}</p>
-    </a>
+    <li className="h-full">
+      <a
+        href={disabled ? null : href}
+        className={`${
+          disabled
+            ? "text-gray-500"
+            : "hover:ring hover:ring-primary-light focus:outline-none focus:ring focus:ring-primary-light"
+        } flex flex-col items-center h-full justify-center bg-gray-200 rounded-xl p-4 space-y-1`}
+      >
+        <h2 className="text-center font-display font-semibold text-xl">
+          {title}
+        </h2>
+        <p className="text-center">{disabled ? "Cooming soon" : description}</p>
+      </a>
+    </li>
   );
 }
