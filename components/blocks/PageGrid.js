61ac6817c491c8f54e737f05618efccc5cb0b4be
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-26 14:46:37 +0200

    Make PageGrid a list

--- components/blocks/PageGrid.js
+++ components/blocks/PageGrid.js
@@ -1,7 +1,7 @@
 export default function PageGrid({ children }) {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-4 items-center lg:-mx-4">
+    <ul className="grid grid-cols-2 md:grid-cols-3 gap-4 items-center lg:-mx-4">
       {children}
-    </div>
+    </ul>
   );
 }
