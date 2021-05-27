ea2b4f5c26a922c631a0ed30fbc8fae478e1496f
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-28 00:27:44 +0200

    Test using local url for og:image

--- components/blocks/Meta.js
+++ components/blocks/Meta.js
@@ -8,10 +8,7 @@ export default function Meta({ title, description, social }) {
       <meta property="og:title" content={title}></meta>
       <meta property="og:site_name" content="neurodiversity.wiki"></meta>
       <meta property="og:description" content={description}></meta>
-      <meta
-        property="og:image"
-        content={"https://neurodiversity.wiki" + social}
-      ></meta>
+      <meta property="og:image" content={social}></meta>
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:site" content="@alvarlagerlof"></meta>
       <meta name="twitter:creator" content="@alvarlagerlof"></meta>
