66942ef0efd038f66fa82fb6cdc5813d8610fa16
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-28 00:44:41 +0200

    Test dyanmic base path

--- components/blocks/Meta.js
+++ components/blocks/Meta.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
 export default function Meta({ title, description, social }) {
+  const basePath = process.env.VERCEL ? process.env.VERCEL_URL : "";
+
   return (
     <Head>
       <title>{title}</title>
@@ -8,7 +10,7 @@ export default function Meta({ title, description, social }) {
       <meta property="og:title" content={title}></meta>
       <meta property="og:site_name" content="neurodiversity.wiki"></meta>
       <meta property="og:description" content={description}></meta>
-      <meta property="og:image" content={social}></meta>
+      <meta property="og:image" content={basePath + social}></meta>
       <meta name="twitter:card" content="summary_large_image"></meta>
       <meta name="twitter:site" content="@alvarlagerlof"></meta>
       <meta name="twitter:creator" content="@alvarlagerlof"></meta>
