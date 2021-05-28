58e70954d8ba3965dd30d21d48c7d6102897e631
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-28 17:50:39 +0200

    Add test callback

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,7 @@ export default function Index({ redirectOrigin }) {
       props: {
         origin: "notocd.com",
       },
+      callback: () => console.log("sent event"),
     });
 
     if (redirectOrigin) {
