ff363e9278fc04d522ffbbb06da1e4abf257838c
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-05-29 02:13:53 +0200

    Set image sizes

--- components/blocks/IconHeading.js
+++ components/blocks/IconHeading.js
@@ -1,7 +1,7 @@
 export default function IconHeading({ url, alt, children }) {
   return (
     <div className="flex flex-row space-x-2 mb-0">
-      <img src={url} alt={alt} />
+      <img src={url} alt={alt} width="24px" height="24px" />
       <h3 className="font-display text-xl md:text-2xl">{children}</h3>
     </div>
   );
