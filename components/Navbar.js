2cec5542dd73c92118518cfcf85e75ef90426445
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-06-02 01:37:41 +0200

    Make nav image clickable

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,18 +2,20 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row space-x-4 items-center max-w-3xl mb-16 md:mb-24 w-full">
-      <img
-        className="w-8 md:w-10"
-        src="/logos/neurodiversity.svg"
-        alt="Logo showing head on purple background"
-        width="32px"
-        height="32px"
-      />
+    <nav className="max-w-3xl mb-16 md:mb-24 w-full">
       <Link href="/">
-        <h1 className="font-display font-semibold text-xl md:text-2xl cursor-pointer">
-          neurodiversity.wiki
-        </h1>
+        <div className="flex flex-row space-x-4 items-center">
+          <img
+            className="w-8 md:w-10 cursor-pointer"
+            src="/logos/neurodiversity.svg"
+            alt="Logo showing head on purple background"
+            width="32px"
+            height="32px"
+          />
+          <h1 className="font-display font-semibold text-xl md:text-2xl cursor-pointer">
+            neurodiversity.wiki
+          </h1>
+        </div>
       </Link>
     </nav>
   );
