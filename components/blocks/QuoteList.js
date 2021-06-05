366ae2e85be9aebac35600c164329e9022aceb71
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-06-05 16:12:49 +0200

    Separate Quote and QuoteList from mdx

--- components/blocks/QuoteList.js
+++ components/blocks/QuoteList.js
@@ -1,14 +1,3 @@
-import Quote from "../Quote";
-
-export default function QuoteList({ quotes }) {
-  return (
-    <ul className="mb-4 flex flex-row flex-wrap gap-2">
-      {quotes &&
-        quotes.map((content) => (
-          <li key={content}>
-            <Quote>{content}</Quote>
-          </li>
-        ))}
-    </ul>
-  );
+export default function QuoteList({ children }) {
+  return <ul className="mb-4 flex flex-row flex-wrap gap-2">{children}</ul>;
 }
