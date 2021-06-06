37b29921c7ca74e5322537fb05162ad3d6036ea0
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-06-06 23:26:49 +0200

    Initial ptsd page

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,12 @@ export default function Index() {
             description="Obsessive-compulsive disorder"
           />
           <PageLink
+            href="/ptsd"
+            title="PTSD"
+            description="Post-traumatic stress disorder"
+            disabled
+          />
+          <PageLink
             href="/dyslexia"
             title="Dyslexia"
             description="Dyslexia"
