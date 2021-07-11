47c91896e40ff5aa2dc0027d5eb893fc8dbc8685
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-11 15:58:03 +0200

    Update env vars

--- components/PreviewBanner.js
+++ components/PreviewBanner.js
@@ -1,11 +1,10 @@
 import ExternalLink from "./ExternalLink";
 
 export default function PreviewBanner({ googleDocUrl }) {
-  if (process.env.VERCEL_GIT_COMMIT_REF !== "main") {
+  if (process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF !== "main") {
     return (
       <div className="bg-gray-200 p-4 lg:-mx-4 rounded-xl max-w-3xl mb-8 w-full">
         <strong>This is a preview page</strong>
-        <p>Test: {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}</p>
         <p className="mb-2">
           The contents of this page is not complete. Do not share the url
           widely.
