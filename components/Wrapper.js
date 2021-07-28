65cc80e077d50d7d3c7ba47d88f145e8350aef97
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-29 00:50:37 +0200

    Colors

--- components/Wrapper.js
+++ components/Wrapper.js
@@ -5,7 +5,7 @@ import { PlausibleProvider } from "../components/Plausible";
 export default function Wrapper({ children }) {
   return (
     <PlausibleProvider>
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col min-h-screen bg-secondary">
         <div className="flex-1 flex flex-col items-center py-8 px-4">
           <Navbar />
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,24 +4,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      display: ["Cabin", "sans-serif"],
-      content: ["Inter", "sans-serif"],
-      displaynew: ["Space Grotesk", "sans-serif"],
+      display: ["Space Grotesk", "sans-serif"],
+      sans: ["Inter", "sans-serif"],
     },
     extend: {
       colors: {
-        primary: {
-          light: "#D49DFF",
-          dark: "#831DD3",
-        },
-        secondary: {
-          light: "#FFF6E0",
-          dark: "#FFDE89",
-        },
-        new: {
-          primary: "#860091",
-          secondary: "#FFEEE5",
-        },
+        primary: "#860091",
+        secondary: "#FFEEE5",
       },
     },
   },
