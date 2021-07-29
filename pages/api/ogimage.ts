d2d11635259bb8f0983df8d8d60722c7ffae6823
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-29 04:08:18 +0200

    Lower cache time

--- pages/api/ogimage.ts
+++ pages/api/ogimage.ts
@@ -24,12 +24,13 @@ export default async function ticketImages(
   const url = `https://${
     process.env.NEXT_PUBLIC_VERCEL_URL
   }/ogimage/?title=${req.query.title.toString()}&description=${req.query.description.toString()}`;
+  const cacheSeconds = 7 * 24 * 60 * 60;
 
   const file = await screenshot(url);
   res.setHeader("Content-Type", `image/png`);
   res.setHeader(
     "Cache-Control",
-    `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    `public, immutable, no-transform, s-maxage=${cacheSeconds}, max-age=${cacheSeconds}`
   );
   res.statusCode = 200;
   res.end(file);
