c6dbff676ea14f7fc15fbbc2c56a8b9f19667c6e
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-29 04:37:32 +0200

    Update logomark

--- pages/ogimage.tsx
+++ pages/ogimage.tsx
@@ -1,7 +1,7 @@
 export default function OgImage({ title, description }) {
   return (
     <div className="w-[2000px] h-[1200px] bg-secondary py-52 px-32 flex flex-col justify-between">
-      <img src="/logos/logomark.svg" className="w-[900px]" />
+      <img src="/logos/neurodiversity/logomark.svg" className="w-[900px]" />
       <div className="space-y-8">
         <h1 className="text-[11em] font-display leading-none">{title}</h1>
         <h2 className="text-[5.2em] leading-tight font-medium">
