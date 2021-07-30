68d08996ac8454b2e133fb5947f3a46a8d376fad
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-30 22:26:09 +0200

    Top bar thin

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,3 +1,3 @@
 export default function TopBar() {
-  return <div className="h-2 sm:h-1 w-full bg-top-bar bg-cover bg-center" aria-hidden />;
+  return <div className="h-1 w-full bg-top-bar bg-cover bg-center" aria-hidden />;
 }
