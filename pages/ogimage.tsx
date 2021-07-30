4a91a2238f70ad042f6e0c74195e526464a1557f
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-30 14:55:03 +0200

    Eslint alt

--- pages/ogimage.tsx
+++ pages/ogimage.tsx
@@ -1,7 +1,11 @@
 export default function OgImage({ title, description }) {
   return (
     <div className="w-[2000px] h-[1200px] bg-secondary py-52 px-32 flex flex-col justify-between">
-      <img src="/logos/neurodiversity/logomark.svg" className="w-[900px]" />
+      <img
+        src="/logos/neurodiversity/logomark.svg"
+        className="w-[900px]"
+        alt=""
+      />
       <div className="space-y-8">
         <h1 className="text-[11em] font-display leading-none">{title}</h1>
         <h2 className="text-[5.2em] font-medium leading-snug">{description}</h2>
