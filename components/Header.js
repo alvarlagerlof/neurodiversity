0fe7ea77bbcbc3f53b66313cb295786df2acb670
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-30 18:24:30 +0200

    Header responsive

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 export default function Header({ children }) {
   return (
-    <header className="flex flex-col justify-center text-center">
+    <header className="flex flex-col justify-center sm:text-center">
       {children}
     </header>
   );
