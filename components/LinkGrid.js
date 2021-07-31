4903d67943ac75e3af707043493d7e11a3336b73
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-31 03:49:22 +0200

    Mobile LinkGrid spacing

--- components/LinkGrid.js
+++ components/LinkGrid.js
@@ -2,7 +2,11 @@ import Typography from "components/Typography";
 import Bounce from "components/Bounce";
 
 function LinkGrid({ children }) {
-  return <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4 lg:-mx-4">{children}</ul>;
+  return (
+    <ul className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-4 lg:-mx-4">
+      {children}
+    </ul>
+  );
 }
 
 function Item({ href, title, description }) {
