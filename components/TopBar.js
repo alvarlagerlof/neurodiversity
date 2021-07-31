e86d78f1ae0a2f4f5e6f090d9b52bac96259ad22
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-07-31 03:46:18 +0200

    Top bar thicker

--- components/TopBar.js
+++ components/TopBar.js
@@ -1,3 +1,3 @@
 export default function TopBar() {
-  return <div className="h-1 w-full bg-top-bar bg-cover bg-center" aria-hidden />;
+  return <div className="h-[7px] w-full bg-top-bar bg-cover bg-center" aria-hidden />;
 }
