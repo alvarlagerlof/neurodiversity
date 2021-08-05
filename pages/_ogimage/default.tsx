5bed24439440c851f8ffee154cfac5b2f6a5b513
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2021-08-05 19:29:32 +0200

    Slightly adjust opengraph images

--- pages/_ogimage/default.tsx
+++ pages/_ogimage/default.tsx
@@ -10,10 +10,10 @@ export default function OgImageLayout() {
 
   return (
     <div className="w-[1200px] h-[630px] fixed top-0 left-0 bg-secondary py-20 px-24 flex flex-col justify-between">
-      <img src="/logos/logomark-primary.svg" className="w-[450px]" alt="" />
+      <img src="/logos/logomark-primary.svg" className="w-[500px]" alt="" />
       <div className="space-y-4">
         <h1 className="text-[6em] font-display font-extrabold leading-none">{title}</h1>
-        <h2 className="text-[2.4em] font-medium leading-snug">{description}</h2>
+        <h2 className="text-[2.7em] font-medium leading-snug">{description}</h2>
       </div>
     </div>
   );
