3f2c1d52a7ce5cbc5267234bd78780dc045725f0
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-03-30 12:57:56 +0200

    Capitialization

--- tests/external.navigation.spec.ts
+++ tests/external.navigation.spec.ts
@@ -1,35 +1,35 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("external navigation", () => {
-  test("should navigate to discord invite on start page", async ({ page }) => {
+  test("should navigate to Discord invite on start page", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await page.locator("text=Join Discord server").click();
 
     await expect(page).toHaveURL("https://discord.com/invite/48kqk6KcZ8");
   });
 
-  test("should navigate to discord invite on join page", async ({ page }) => {
+  test("should navigate to Discord invite on join page", async ({ page }) => {
     await page.goto("/join");
     await page.locator("text=Join Discord server").click();
 
     await expect(page).toHaveURL("https://discord.com/invite/48kqk6KcZ8");
   });
 
-  test("should navigate to discord invite from footer", async ({ page }) => {
+  test("should navigate to Discord invite from footer", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await page.locator("footer section div section > div > div a:nth-child(3)").click();
 
     await expect(page).toHaveURL("https://discord.com/invite/48kqk6KcZ8");
   });
 
-  test("should navigate to twitter from footer", async ({ page }) => {
+  test("should navigate to Twitter from footer", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await page.locator("footer section div section > div > div a:nth-child(1)").click();
 
     await expect(page).toHaveURL("https://twitter.com/n12ywiki");
   });
 
-  test("should navigate to github from footer", async ({ page }) => {
+  test("should navigate to Github from footer", async ({ page }) => {
     await page.goto("http://localhost:3000/");
     await page.locator("footer section div section > div > div a:nth-child(2)").click();
 
