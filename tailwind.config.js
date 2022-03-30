c704015c59183bfce7e02431306c90ec1f570ff7
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-03-31 00:04:26 +0200

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   mode: "jit",
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media",
   theme: {
     fontFamily: {
       display: ["Inter var", ...defaultTheme.fontFamily.sans],
