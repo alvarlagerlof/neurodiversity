c6b86335375a78f739ea93e4b3d2bed0a39f1de8
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-03-30 02:30:59 +0200

    More playwright

---
+++ tests/internal.navigation.spec.ts
@@ -0,0 +1,35 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("internal navigation", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+  });
+
+  test("should navigate to the Autism page", async ({ page }) => {
+    await page.locator("text=ASD").click();
+
+    await expect(page).toHaveURL("http://localhost:3000/autism");
+    await expect(page.locator("h1")).toContainText("What is Autism?");
+  });
+
+  test("should navigate to the DID page", async ({ page }) => {
+    await page.locator("text=DID").click();
+
+    await expect(page).toHaveURL("http://localhost:3000/did");
+    await expect(page.locator("h1")).toContainText("What is DID?");
+  });
+
+  test("should navigate to the OCD page", async ({ page }) => {
+    await page.locator("text=OCD").click();
+
+    await expect(page).toHaveURL("http://localhost:3000/ocd");
+    await expect(page.locator("h1")).toContainText("What is OCD?");
+  });
+
+  test("should navigate to the join page", async ({ page }) => {
+    await page.locator("text=Find out more").click();
+
+    await expect(page).toHaveURL("http://localhost:3000/join");
+    await expect(page.locator("h1")).toContainText("Join us");
+  });
+});
