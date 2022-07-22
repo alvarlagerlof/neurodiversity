2d3046a76eb5366873909073d013ad755cfa0c5c
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-07-22 02:11:44 +0200

    List

--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -53,64 +53,72 @@ export default function Calendar({ events }: CalendarProps) {
           </Header>
 
           <Main>
-            <Section
+            {/* <Section
               className={`
               h-full space-x-2 rounded-xl p-4 bg-white ring-primary transition
               -mx-4
               shadow`}
-            >
-              <ul className="space-y-8">
-                {Object.entries(events)
-                  .sort((a: any, b: any) => a[0] - b[0])
-                  .map(([month, events]) => {
-                    const date = new Date(2000, parseInt(month), 0); // 2009-11-10
-                    const monthName = date.toLocaleString("default", { month: "long" });
+            > */}
+            <ul className="space-y-8">
+              {Object.entries(events)
+                .sort((a: any, b: any) => a[0] - b[0])
+                .map(([month, events]) => {
+                  const date = new Date(2000, parseInt(month), 0); // 2009-11-10
+                  const monthName = date.toLocaleString("default", { month: "long" });
 
-                    return (
-                      <li key={month}>
-                        <Typography.Heading className="ml-[170px]">
-                          {monthName}
-                        </Typography.Heading>
-                        <ul className="space-y-2">
-                          {events.map((event) => {
-                            const pr = new Intl.PluralRules("en-US", { type: "ordinal" });
+                  return (
+                    <li key={month}>
+                      <Typography.Title as="h2">{monthName}</Typography.Title>
+                      <ul className="space-y-4">
+                        {events.map((event) => {
+                          const pr = new Intl.PluralRules("en-US", { type: "ordinal" });
 
-                            const suffixes = new Map([
-                              ["one", "st"],
-                              ["two", "nd"],
-                              ["few", "rd"],
-                              ["other", "th"],
-                            ]);
-                            const formatOrdinals = (n) => {
-                              const rule = pr.select(n);
-                              const suffix = suffixes.get(rule);
-                              return `${n}${suffix}`;
-                            };
+                          const suffixes = new Map([
+                            ["one", "st"],
+                            ["two", "nd"],
+                            ["few", "rd"],
+                            ["other", "th"],
+                          ]);
+                          const formatOrdinals = (n) => {
+                            const rule = pr.select(n);
+                            const suffix = suffixes.get(rule);
+                            return `${n}${suffix}`;
+                          };
 
-                            const day = formatOrdinals(
-                              new Date(event.startDate).getDay()
-                            );
+                          const day = formatOrdinals(new Date(event.startDate).getDay());
 
-                            return (
-                              <li key={event.name} className="flex flex-row items-center">
-                                <div className="w-[170px] flex flex-row justify-end pr-4">
-                                  {event.length == "day" && <Tag>{day}</Tag>}
-                                  {event.length == "week" && (
-                                    <Tag>Week, starting {day}</Tag>
-                                  )}
-                                  {event.length == "month" && <Tag>Whole month</Tag>}
-                                </div>
+                          return (
+                            <li
+                              key={event.name}
+                              className="
+                                h-full space-x-2 rounded-xl p-4 bg-white ring-primary transition
+                                -mx-4
+                                shadow"
+                            >
+                              <div className="ml-2 mb-2">
+                                {event.length == "day" && <Tag>{day}</Tag>}
+                                {event.length == "week" && (
+                                  <Tag>Week, starting {day}</Tag>
+                                )}
+                                {event.length == "month" && <Tag>Whole month</Tag>}
+                              </div>
 
-                                <p className="mr-4">{event.name}</p>
-                              </li>
-                            );
-                          })}
-                        </ul>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </Section>
+                              <Typography.Heading>{event.name}</Typography.Heading>
+                              <p className="mb-4">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Accusantium, fugiat molestiae. Iure, ab nisi suscipit
+                                commodi optio, qui.
+                              </p>
+                              <ExternalLink href="">Learn more</ExternalLink>
+                            </li>
+                          );
+                        })}
+                      </ul>
+                    </li>
+                  );
+                })}
+            </ul>
+            {/* </Section> */}
           </Main>
         </VerticalSpacer>
       </ContentInset>
