18b4905e6ac0d5860168bee0038c6994e769eed2
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-11-01 00:46:22 +0100

    switch out localhost

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,9 +1,11 @@
 import { test, expect } from "@playwright/test";
 
+const targetUrl = process.env.ENVIRONMENT_URL;
+
 test.describe("basic flow", () => {
   // Donation button is hidden
   // test("it should open the dropdown and close when clicking", async ({ page }) => {
-  //   await page.goto("http://localhost:3000/");
+  //   await page.goto(targetUrl;
 
   //   const menuButton = await page.locator("#menu-button");
   //   await menuButton.click();
@@ -14,16 +16,16 @@ test.describe("basic flow", () => {
   // });
 
   test("should navigate to the Autism page", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto(targetUrl);
 
     await page.locator("text=ASD").click();
 
-    await expect(page).toHaveURL("http://localhost:3000/autism");
+    await expect(page).toHaveURL(`${targetUrl}/autism`);
     await expect(page.locator("h1")).toContainText("What is Autism?");
   });
 
   test("it should open and close the definition when clicking", async ({ page }) => {
-    await page.goto("http://localhost:3000/asd");
+    await page.goto(`${targetUrl}/asd`);
 
     const dt = await page.locator("dt >> nth=0");
     const dd = await page.locator("dd >> nth=0");
@@ -38,7 +40,7 @@ test.describe("basic flow", () => {
   });
 
   test("it should go to the join page and click the discord link", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto(targetUrl);
 
     const button = await page.locator("text=Join Discord server");
     await button.evaluate(async (element) => await element.removeAttribute("target"));
