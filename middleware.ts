d784645ec7c56ccf7d5f0f9d363205ce638f070b
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-01-15 02:02:16 +0100

    Add www to notocd redirect
    Changed the domain setup.

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.headers.get("Host") == "notocd.com") {
+  if (request.headers.get("Host") == "www.notocd.com") {
     return new NextResponse(null, {
       status: 301,
       headers: {
