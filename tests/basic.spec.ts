d5e49fce858133168ba2eb1f348069ad22b7418d
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-02-05 23:44:09 +0100

    Add Checkly workflow
    Runs the E2E test on Checkly instead of localhost Playwright

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,29 +1,17 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("basic flow", () => {
-  // Donation button is hidden
-  // test("it should open the dropdown and close when clicking", async ({ page }) => {
-  //   await page.goto("http://localhost:3000/");
-
-  //   const menuButton = await page.locator("#menu-button");
-  //   await menuButton.click();
-  //   await expect(menuButton).toHaveAttribute("aria-expanded", "true");
-
-  //   await menuButton.click();
-  //   await expect(menuButton).toHaveAttribute("aria-expanded", "false");
-  // });
-
   test("should navigate to the Autism page", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto(process.env.ENVIRONMENT_URL);
 
     await page.locator("text=ASD").click();
 
-    await expect(page).toHaveURL("http://localhost:3000/autism");
+    await expect(page).toHaveURL(`${process.env.ENVIRONMENT_URL}/autism`);
     await expect(page.locator("h1")).toContainText("What is Autism?");
   });
 
   test("it should open and close the definition when clicking", async ({ page }) => {
-    await page.goto("http://localhost:3000/asd");
+    await page.goto(`${process.env.ENVIRONMENT_URL}/asd`);
 
     const dt = await page.locator("dt >> nth=0");
     const dd = await page.locator("dd >> nth=0");
@@ -38,7 +26,7 @@ test.describe("basic flow", () => {
   });
 
   test("it should go to the join page and click the discord link", async ({ page }) => {
-    await page.goto("http://localhost:3000/");
+    await page.goto(`${process.env.ENVIRONMENT_URL}`);
 
     const button = await page.locator("text=Join Discord server");
     await button.evaluate(async (element) => await element.removeAttribute("target"));
