f7c6e5da49b9da2bddfec71d43e487800ba0cd16
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-02-06 01:28:01 +0100

    Fix link accessbility issue
    Seems that <lI> needs to be a direct child of <ul>

--- app/components/LinkGridItem.tsx
+++ app/components/LinkGridItem.tsx
@@ -16,41 +16,43 @@ export function LinkGridItem({ href, title, description }: Props) {
   const link = useRef(null);
 
   return (
-    <Bounce amount={1.04}>
-      <li
-        onClick={(e) => {
-          if (link.current && link.current !== e.target) {
-            link.current.click();
-          }
-        }}
-        className={`
-                h-full flex flex-row items-start space-x-2 rounded-xl p-4 bg-white dark:bg-card-dark ring-primary transition
+    <li>
+      <Bounce amount={1.04} className="w-full h-full">
+        <div
+          onClick={(e) => {
+            if (link.current && link.current !== e.target) {
+              link.current.click();
+            }
+          }}
+          className={`
+                h-full w-full flex flex-row items-start space-x-2 rounded-xl p-4 bg-white dark:bg-card-dark ring-primary transition
                 shadow
                 hover:shadow-md
                 outline-none focus-visible:ring cursor-pointer`}
-      >
-        <img
-          src="/icons/arrow-light.svg"
-          className="w-6 md:w-7 dark:hidden"
-          alt=""
-          aria-hidden
-        />
-        <img
-          src="/icons/arrow-dark.svg"
-          className="w-6 md:w-7 hidden dark:block"
-          alt=""
-          aria-hidden
-        />
+        >
+          <img
+            src="/icons/arrow-light.svg"
+            className="w-6 md:w-7 dark:hidden"
+            alt=""
+            aria-hidden
+          />
+          <img
+            src="/icons/arrow-dark.svg"
+            className="w-6 md:w-7 hidden dark:block"
+            alt=""
+            aria-hidden
+          />
 
-        <div>
-          <Typography.Heading className="mb-1">
-            <Link href={href} ref={link}>
-              {title}
-            </Link>
-          </Typography.Heading>
-          <Typography.Body className="mb-0">{description}</Typography.Body>
+          <div>
+            <Typography.Heading className="mb-1">
+              <Link href={href} ref={link}>
+                {title}
+              </Link>
+            </Typography.Heading>
+            <Typography.Body className="mb-0">{description}</Typography.Body>
+          </div>
         </div>
-      </li>
-    </Bounce>
+      </Bounce>
+    </li>
   );
 }
