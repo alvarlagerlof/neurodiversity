a3eee2935512a68625e37640c7dab109af554b64
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-02-26 14:58:08 +0100

    Remove console.log

--- pages/api/og/start.tsx
+++ pages/api/og/start.tsx
@@ -50,7 +50,6 @@ export default async function handler() {
       }
     );
   } catch (e: any) {
-    console.log(`${e.message}`);
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
