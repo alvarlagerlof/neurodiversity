040cdfc2f77bd7e559304187a4d4f9a5a550ea5f
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-02-26 15:01:22 +0100

    Remove title suffix for og:image in /[slug]

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -22,7 +22,7 @@ export function generateMetadata({
       images: `https://${
         process.env.NEXT_PUBLIC_VERCEL_URL
       }/api/og/default?title=${encodeURIComponent(
-        `${page.meta.title} - Neurodiversity.wiki`
+        `${page.meta.title}`
       )}&description=${encodeURIComponent(
         `${page.meta.description} Learn more here.`
       )}`,
